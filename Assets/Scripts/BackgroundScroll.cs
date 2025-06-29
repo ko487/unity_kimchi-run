e8c662a40387a0b033c09e5c9d2ac785ae147a6c
ko487 <kxcnms@yonsei.ac.kr>
2025-06-29 15:02:50 +0900

    start
    플레이어 객체 생성까지

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("How fast shoud the  texture scroll?")]
+    public float scrollSpeed;
+
+    [Header("References")]
+    public MeshRenderer meshRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()   // i want 10 units per secen ||| 2 fps
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime, 0);
+    }
+}
